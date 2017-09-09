41b4e2a4d75b9f21f95ffd5a5bb7f4f53d722836
minipikawho <31785904+minipikawho@users.noreply.github.com>
2017-09-09 11:51:23 -0400

    Add files via upload

--- HackathonDataStuff.py
+++ HackathonDataStuff.py
@@ -6,32 +6,77 @@ tripFile = file.read()
 file.close
 
 userAndTrip = []
+users = []
+trips = []
 userAllLists = userFile.split("\n")
 tripAllLists = tripFile.split("\n")
+
 for i in userAllLists:
-    for x in tripAllLists:
-        if i[0] == x[1]:
-            userAndTrip.append(i + x)
-            print('{"user":[')
-            print('{"breezecard_id":"' + str(userAndTrip[0]) +'",')
-            print('"first_name":"' + str(userAndTrip[1]) +'",')
-            print('"last_name":"' + str(userAndTrip[2]) + '",')
-            print('"dob":"' + str(userAndTrip[3]) + '",')
-            print('"date_joined":"' + str(userAndTrip[4]) + '",')
-            print('"username":"' + str(userAndTrip[5]) + '",')
-            print('"password":"' + str(userAndTrip[6]) +'",')
-            print('"trips": [')
-            print('{"trip_id":"' + str(userAndTrip[7]) + '",')
-            print('"Breezecard_id":"' + str(userAndTrip[8]) + '",')
-            print('"stop_enter_name":"' + str(userAndTrip[9]) + '",')
-            print('"stop_enter_lat":' + str(userAndTrip[10]) +',')
-            print('"stop_enter_long":' + str(userAndTrip[11]) + ',')
-            print('"stop_exit_name":"' + str(userAndTrip[12]) + '",')
-            print('"stop_exit_lat":' + str(userAndTrip[13]) + ',')
-            print('"stop_exit_long":' + str(userAndTrip[14]) + ',')
-            print('"trip_date":"' + str(userAndTrip[15]) + '",')
-            print('"trip_mileage":' + str(userAndTrip[16]) + ',')
-            print('"trip_duration":' + str(userAndTrip[17]) + ',')
-            print('"fare":' + str(userAndTrip[18]) + '} ]')
-            print('} ]')
-            print('}')
+    x = i.split(",")
+    users.append(x)
+
+for i in tripAllLists:
+    x = i.split(",")
+    trips.append(x)
+
+jsonList = []
+jsonList.append('{"user":[')
+for i in users:
+    x = '{"breezecard_id":"' + str(i[0]) + '",' \
+                '"first_name":"' + str(i[1]) + '",' \
+                '"last_name":"' + str(i[2]) + '",' \
+                '"dob":"' + str(i[3]) + '",' \
+                '"date_joined":"' + str(i[4]) + '",' \
+                '"username":"' + str(i[5]) + '",' \
+                '"password":"' + str(i[6]) + '",' \
+                '"trips": ['
+    jsonList.append(x)
+    for j in trips:
+        if i[0] == j[1]:
+            y = '{"trip_id":"' + str(j[0]) + '",' \
+                '"Breezecard_id":"' + str(j[1]) + '",' \
+                '"Route_Type":"' +str(j[2]) + '",' \
+                '"stop_enter_name":"' + str(j[3]) + '",' \
+                '"stop_enter_lat":' + str(j[4]) +',' \
+                '"stop_enter_long":' + str(j[5]) + ',' \
+                '"stop_exit_name":"' + str(j[6]) + '",' \
+                '"stop_exit_lat":' + str(j[7]) + ',' \
+                '"stop_exit_long":' + str(j[8]) + ',' \
+                '"trip_date":"' + str(j[9]) + '",' \
+                '"trip_mileage":' + str(j[10]) + ',' \
+                '"trip_duration":' + str(j[11]) + ',' \
+                '"fare":' + str(j[12]) + '},'               
+            jsonList.append(y)
+
+f = open("output.text", "w")
+for item in jsonList:
+    f.write(item + "\n")
+f.close()
+
+#            for x in trips:
+#     if i[0] == x[1]:
+#         userAndTrip.append(i + x)       
+##            for z in userAndTrip:               
+##                print('{"user":[' \
+##                '{"breezecard_id":"' + str(z[0]) + '",' \
+##                '"first_name":"' + str(z[1]) + '",' \
+##                '"last_name":"' + str(z[2]) + '",' \
+##                '"dob":"' + str(z[3]) + '",' \
+##                '"date_joined":"' + str(z[4]) + '",' \
+##                '"username":"' + str(z[5]) + '",' \
+##                '"password":"' + str(z[6]) + '",' \
+##                '"trips": [' \
+##                '{"trip_id":"' + str(z[7]) + '",' \
+##                '"Breezecard_id":"' + str(z[8]) + '",' \
+##                '"stop_enter_name":"' + str(z[9]) + '",' \
+##                '"stop_enter_lat":' + str(z[10]) +',' \
+##                '"stop_enter_long":' + str(z[11]) + ',' \
+##                '"stop_exit_name":"' + str(z[12]) + '",' \
+##                '"stop_exit_lat":' + str(z[13]) + ',' \
+##                '"stop_exit_long":' + str(z[14]) + ',' \
+##                '"trip_date":"' + str(z[15]) + '",' \
+##                '"trip_mileage":' + str(z[16]) + ',' \
+##                '"trip_duration":' + str(z[17]) + ',' \
+##                '"fare":' + str(z[18]) + '} ]' \
+##                '} ]' \
+##                '}')
